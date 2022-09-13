d2e9488ab9cb7132c826f54606957e43820faddc
Ahmed Mongy <a.elzantot@gmail.com>
2022-09-13 19:22:23 +0200

    install and configure tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import {View} from 'react-native';
+import React from 'react';
+import {Text, View} from 'react-native';
 
 const App = () => {
-  return <View />;
+  return (
+    <View className="flex-1 items-center justify-center bg-fuchsia-500">
+      <Text className="text-green-500 font-black">Hello</Text>
+    </View>
+  );
 };
 
 export default App;
